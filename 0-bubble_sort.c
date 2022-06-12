c7c642d915bd8c116b83a08f32b8718621cae6c0
jaafarfares <jaafarferes@gmail.com>
2022-06-12 20:56:31 +0100

    first test

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,13 +22,13 @@ int temp = *x;
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
+size_t i, j;
 
-    for (i = 0; i < size - 1; i++)
-        for (j = 0; j < size - i - 1; j++)
-            if (array[j] > array[j + 1])
-                {
-                    swap(&array[j], &array[j +1]);
-                    print_array(array, size);
-                }
+for (i = 0; i < size - 1; i++)
+for (j = 0; j < size - i - 1; j++)
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+print_array(array, size);
+}
 }

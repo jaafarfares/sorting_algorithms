a3d8f892c6b11f91128b4f464093cb0c7e0e921f
jaafarfares <jaafarferes@gmail.com>
2022-06-12 20:35:11 +0100

    first test

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ *swap - ....
+ *
+ * @x : ...
+ * @y : ...
+ *Return:...
+ */
+void swap(int *x, int *y)
+{
+int temp = *x;
+*x = *y;
+*y = temp;
+}
+
+/**
+ * bubble_sort - the function
+ * @array: ....
+ * @size: ....
+ * Return: ....
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+
+    for (i = 0; i < size - 1; i++)
+        for (j = 0; j < size - i - 1; j++)
+            if (array[j] > array[j + 1])
+                {
+                    swap(&array[j], &array[j + 1]);
+                    print_array(array, size);
+                }
+}

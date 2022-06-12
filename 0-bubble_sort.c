517aa2602b50f303c02e10b23929966adffb9cc9
jaafarfares <jaafarferes@gmail.com>
2022-06-12 21:14:22 +0100

    test

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,8 +22,10 @@ int temp = *x;
 
 void bubble_sort(int *array, size_t size)
 {
-unsigned int i, j;
+size_t i, j;
 
+if (size <= 1)
+return;
 for (i = 0; i < size - 1; i++)
 for (j = 0; j < size - i - 1; j++)
 if (array[j] > array[j + 1])

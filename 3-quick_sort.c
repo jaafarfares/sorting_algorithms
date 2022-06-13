d239c0e3ca74619eef12c7b935cbc01f92e9570c
jaafarfares <jaafarferes@gmail.com>
2022-06-13 10:31:56 +0100

    test

---
+++ 3-quick_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+/**
+ * quick_sort - ....
+ * @array: ...
+ * @size: ...
+ *Return: ...
+ */
+
+void quick_sort(int *array, size_t size)
+{
+size_t mid = size / 2;
+size_t temp;
+
+if (size <= 1)
+return;
+if (array[mid] < array[0])
+{
+temp = array[0];
+array[0] = array[mid];
+array[mid] = temp;
+print_array(array, size);
+}
+if (array[size - 1] < array[0])
+{
+temp = array[0];
+array[0] = array[size - 1];
+array[size - 1] = temp;
+print_array(array, size);
+}
+if (array[mid] < array[size - 1])
+{
+temp = array[mid];
+array[mid] = array[size - 1];
+array[size - 1] = temp;
+print_array(array, size);
+
+}
+print_array(array, size);
+}
